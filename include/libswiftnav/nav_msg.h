42cc0851800003686bc3f5ba3c40227ca783d430
Gareth McMullin <gareth@blacksphere.co.nz>
2015-11-23 12:17:01 -0800

    nav_msg: Fix bit sync for SBAS.

--- include/libswiftnav/nav_msg.h
+++ include/libswiftnav/nav_msg.h
@@ -26,6 +26,17 @@
 #define BIT_POLARITY_UNKNOWN -1
 
 typedef struct {
+  gnss_signal_t sid;
+  u8 bit_length;
+
+  u8 bit_phase;
+  s8 bit_phase_ref;  /**< -1 = not synced.*/
+  s32 bit_integrate;
+
+  u8 bitsync_count;
+  s32 bitsync_prev_corr[20];
+  u32 bitsync_histogram[20];
+
   u32 subframe_bits[NAV_MSG_SUBFRAME_BITS_LEN];
   u16 subframe_bit_index;
   bool overrun;
@@ -35,20 +46,13 @@ typedef struct {
    * - -x = inverse preamble begins at (1-x)
    */
   s16 subframe_start_index;
-  u8 bit_phase;
-  s8 bit_phase_ref;  /**< -1 = not synced.*/
-  s32 bit_integrate;
 
   u32 frame_words[3][8];
   u8 next_subframe_id;
   s8 bit_polarity;
-
-  u8 bitsync_count;
-  s32 bitsync_prev_corr[20];
-  u32 bitsync_histogram[20];
 } nav_msg_t;
 
-void nav_msg_init(nav_msg_t *n);
+void nav_msg_init(nav_msg_t *n, gnss_signal_t);
 s32 nav_msg_update(nav_msg_t *n, s32 corr_prompt_real, u8 ms);
 bool subframe_ready(nav_msg_t *n);
 s8 process_subframe(nav_msg_t *n, ephemeris_t *e);
